84e3c3fefd6948ddfc48c4c555b418af3f82d153
randolrs <r.shane.randolph@gmail.com>
2021-03-05 08:48:56 -0800

    add signup via API

--- pages/user/signup.js
+++ pages/user/signup.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import {useState} from 'react';
+import axios from 'axios';
 
 export default function Signup() {
     const [email, setEmail] = useState('');
 		const [password, setPassword] = useState('');
 		console.log({email, password});
-		function handleSignup() {
-			console.log({email, password});
-		}
+	function handleSignup() {
+		console.log({email, password});
+		axios.post('https://comedy.auth0.com/dbconnections/signup', {
+			"client_id": "svj4B2yH44IKctWJUi6jXmMxGrj4JJZG",
+			email,
+  			password,
+  			"connection": "Username-Password-Authentication",
+		});
+	}
 		
     return (
     <div>
